2e2073393938781c8e897bca6b8c46bf4204dc62
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-06-27 19:07:46 +0200

    added about page to play a little with the new router :)

--- src/ASPNETCoreRC2Angular2Demo/wwwroot/app/app.routes.js
+++ src/ASPNETCoreRC2Angular2Demo/wwwroot/app/app.routes.js
@@ -1,8 +1,10 @@
 "use strict";
 var router_1 = require('@angular/router');
 var home_component_1 = require('./home/home.component');
+var about_component_1 = require('./about/about.component');
 exports.routes = [
-    { path: '', component: home_component_1.HomeComponent }
+    { path: '', component: home_component_1.HomeComponent },
+    { path: 'about', component: about_component_1.AboutComponent },
 ];
 exports.APP_ROUTER_PROVIDERS = [
     router_1.provideRouter(exports.routes)
