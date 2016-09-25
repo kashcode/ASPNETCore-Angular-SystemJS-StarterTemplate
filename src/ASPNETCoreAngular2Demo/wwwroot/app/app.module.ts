3a79c53dfe7a9dc9e015c5076701457e4dc8709b
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-09-25 12:38:34 +0200

    small cleanups

--- src/ASPNETCoreAngular2Demo/wwwroot/app/app.module.ts
+++ src/ASPNETCoreAngular2Demo/wwwroot/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { Configuration } from './app.constants';
 import { routing, appRoutingProviders } from './app.routes';
 import { HttpModule, JsonpModule } from '@angular/http';
@@ -21,11 +21,12 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     ],
 
     declarations: [
-        AppComponent, 
-        HomeComponent, 
-        AboutComponent, 
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
         FooterComponent,
-        NavigationComponent],
+        NavigationComponent
+    ],
 
     providers: [
         appRoutingProviders,
