19f11274135de4d5b8414083b833578863545185
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-04-13 22:02:40 +0200

    updating to the latest versions

--- src/ASPNETCoreAngular2Demo/angularApp/app.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var router_1 = require("@angular/router");
-var AppRoutes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
-];
-exports.AppRouting = router_1.RouterModule.forRoot(AppRoutes);
-//# sourceMappingURL=app.routes.js.map
--- src/ASPNETCoreAngular2Demo/angularApp/home/home.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var home_component_1 = require("./components/home/home.component");
-var router_1 = require("@angular/router");
-var routes = [
-    { path: 'home', component: home_component_1.HomeComponent }
-];
-exports.HomeRoutes = router_1.RouterModule.forChild(routes);
-//# sourceMappingURL=home.routes.js.map
